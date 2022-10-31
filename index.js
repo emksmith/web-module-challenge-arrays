d6b7e6d337fe3f6f5e8dd64a00e1b8dcd9c46cf8
emksmith <116416382+emksmith@users.noreply.github.com>
2022-10-30 18:20:31 -0700

    "testing again"

--- index.js
+++ index.js
@@ -64,8 +64,10 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-  /*your code here*/
+function is31Flavors(originalFlavors){
+  if(originalFlavors.length === 31) {
+    return true;
+  }
  }
 
 
